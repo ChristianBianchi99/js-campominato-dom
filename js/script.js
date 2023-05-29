38655e785eb12c619e86abb6e252f7495fa2ae09
ChristianBianchi99 <bianchi848@gmail.com>
2023-05-29 16:32:34 +0200

    Agg array bombs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 // DICHIARAZIONE VARIABILI
 let btn = document.querySelector(".btn")
 let grid = document.querySelector(".grid")
-let square10 = "square-10"
-let square9 = "square-9"
-let square7 = "square-7"
-let square = document.querySelector(".square-10");
+let square = "";
+let cells= "";
+let bombs = [];
 
 // CREAZIONE FUNZIONI
 // Funzione per generare i quadrati
@@ -28,26 +27,51 @@ function gridGenerator(a, b, c){
         })
     }
 }
+//Funzione per generare le bombe
+function bombsGenerator(array, max){
+    let check = false
+    let bomb;
+    while(check === false){
+        bomb = Math.floor(Math.random() * (max - 1 + 1) + 1)
+        
+        if(!array.includes(bomb)){
+            check = true
+        }
+    }
+    return bomb
+}
 
 // IMPOSTAZIONE CLICK BTN
 btn.addEventListener("click", function(){
 
     grid.innerHTML = "";
-    // IMPOSTAZIONE PER LA DIFFICOLTA'
+    bombs = []
+    // IMPOSTAZIONI PER LA DIFFICOLTA'
     let difficulty = document.querySelector(".form-select").value;
     if( difficulty == 1){
         if(grid.innerHTML === ""){
-            gridGenerator(1, 100, square10)
+            cells = 100
+            square = "square-10"
         }
     } else if ( difficulty == 2){
         if(grid.innerHTML === ""){
-            gridGenerator(1, 81, square9)
+            cells = 81
+            square = "square-9"
         }
     } else if ( difficulty == 3){
         if(grid.innerHTML === ""){
-            gridGenerator(1, 49, square7)
+            cells = 49
+            square = "square-7"
         }
     } else {
         alert("Seleziona una difficoltà prima di continuare")
     }
+    //GENERAZIONE GRIGLIA
+    gridGenerator(1, cells, square)
+    //GENERAZIONE BOMBE
+    for(let i=1; i<=16; i++){
+        let bomb=bombsGenerator(bombs, cells)
+        bombs.push(bomb)
+    }
+    console.log(bombs)
 })
